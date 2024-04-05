6254f33bf899e990a30201d9fddbe2ed810c539b
Nikolai Orlov <nicolasorlov174@yandex.com>
2024-04-05 21:25:00 +0200

    fix: updated files for publishing files

--- setup.py
+++ setup.py
@@ -9,4 +9,8 @@ setup(
     version='0.0.1',
     description='Medipix EBSD library',
     author='Nikolai Orlov',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
 )
